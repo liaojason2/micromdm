abb3d67f5cdadbf73f90cf58030ec5367174beaf
Jacob Jensen <jacob-jensen@bethel.edu>
2019-03-13 10:56:19 -0500

    add queue endpoint

---
+++ platform/queue/get_device_command.go
@@ -0,0 +1,37 @@
+package queue
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/go-kit/kit/endpoint"
+
+	"github.com/micromdm/micromdm/pkg/httputil"
+)
+
+func decodeGetDeviceCommandRequest(ctx context.Context, r *http.Request) (interface{}, error) {
+	var req string
+	err := httputil.DecodeJSONRequest(r, &req)
+	return req, err
+}
+
+func decodeGetDeviceCommandResponse(_ context.Context, r *http.Response) (interface{}, error) {
+	var resp DeviceCommand
+	err := httputil.DecodeJSONResponse(r, &resp)
+	return resp, err
+}
+
+func MakeGetDeviceCommandEndpoint(svc Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		udid := request.(string)
+		return DeviceCommand(udid)
+	}
+}
+
+func (e Endpoints) GetDeviceCommand(ctx context.Context, udid string) (DeviceCommand, error) {
+	response, err := e.GetDeviceCommandEndpoint(ctx, udid)
+	if err != nil {
+		return nil, err
+	}
+	return response, err
+}
---
+++ platform/queue/server.go
@@ -0,0 +1,31 @@
+package queue
+
+import (
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+
+	"github.com/micromdm/micromdm/pkg/httputil"
+)
+
+type Endpoints struct {
+	GetDeviceCommandEndpoint endpoint.Endpoint
+}
+
+func MakeServerEndpoints(s Service, outer endpoint.Middleware, others ...endpoint.Middleware) Endpoints {
+	return Endpoints{
+		GetDeviceCommandEndpoint: endpoint.Chain(outer, others...)(MakeGetDeviceCommandEndpoint(s)),
+	}
+}
+
+func RegisterHTTPHandlers(r *mux.Router, e Endpoints, options ...httptransport.ServerOption) {
+	// POST     /v1/queue		get the command queue for a device managed by the server
+
+	r.Methods("POST").Path("/v1/queue").Handler(httptransport.NewServer(
+		e.ListDevicesEndpoint,
+		decodeListDevicesRequest,
+		httputil.EncodeJSONResponse,
+		options...,
+	))
+
+}
