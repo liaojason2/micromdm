7406f8a593e7155b6e12b4e29bdbfb5084aadbfe
Jacob Jensen <jacob-jensen@bethel.edu>
2019-03-13 13:21:35 -0500

    Update service.go

--- platform/queue/service.go
+++ platform/queue/service.go
@@ -8,10 +8,6 @@ type Service interface {
 	GetDeviceCommand(ctx context.Context, udid string) (DeviceCommand, error)
 }
 
-type Store interface {
-	DeviceCommand(udid string) (DeviceCommand, error)
-}
-
 type DeviceCommandService struct {
 	store Store
 }
